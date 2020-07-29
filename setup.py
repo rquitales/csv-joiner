e225b83464307f5d89c51f8560113f3f4d78d74a
Ramon Quitales <ramon@landed.com>
2020-07-29 16:56:27 -0700

    Initialise Python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2020 Ramon Quitales
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from distutils.core import setup
+
+setup(name='CSVJoiner',
+      version='0.1.0',
+      description='Join CSV files through common columns',
+      author='Ramon Quitales',
+      author_email='oss@rquitales.com',
+      url='https://www.github.com/csv-joiner-python',
+      packages=['csvjoiner'],
+      )
